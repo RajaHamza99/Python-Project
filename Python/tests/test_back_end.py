8dd66154af361acc132bb79a5949fd43e256bb1c
Hamza <rajahamza104@gmail.com>
2020-06-19 17:57:19 +0000

    Unit and Integration testing 74%

--- Python/tests/test_back_end.py
+++ Python/tests/test_back_end.py
@@ -4,7 +4,7 @@ from flask import url_for
 from flask_testing import TestCase
 
 from application import app, db, bcrypt
-from application.models import Users, Playlists
+from application.models import Users, Playlists, Songs
 from os import getenv
 
 class TestBase(TestCase):
@@ -50,12 +50,186 @@ class TestBase(TestCase):
         db.session.remove()
         db.drop_all()
 
+
+
+
+def logging_in(self):
+   response =  self.client.post(
+            '/login',
+            data=dict(
+                email = "admin@admin.com",
+                password = "admin2016",               
+                ),
+            follow_redirects=True
+            )
+   return response
+
+
+
 class TestViews(TestBase):
 
     def test_homepage_view(self):
         """
         Test that homepage is accessible without login
         """
-        response = self.client.get(url_for('home'))
+        self.assertEqual(self.client.get(url_for('home')).status_code, 200)
+
+    def test_login(self):
+        self.assertEqual(self.client.get(url_for('login')).status_code, 200)
+
+    def test_about(self):
+        self.assertEqual(self.client.get(url_for('about')).status_code, 200)
+
+    def test_register(self):
+        self.assertEqual(self.client.get(url_for('register')).status_code, 200)
+
+    def test_songs(self):
+        logging_in(self)
+        self.assertEqual(self.client.get(url_for('add_songs')).status_code, 200)
+
+    def test_playlist(self):
+        logging_in(self)
+        self.assertEqual(self.client.get(url_for('playlist')).status_code, 200)
+    
+
+    def test_account(self):
+        logging_in(self)
+        self.assertEqual(self.client.get(url_for('account')).status_code, 200)
+
+    def test_playlist_delete_view(self):
+        response = self.client.post(
+            '/login',
+            data=dict(
+                email="admin@admin.com",
+                password="admin2016",
+                ),
+            follow_redirects=True
+            )
         self.assertEqual(response.status_code, 200)
+    
+        response_two = self.client.get(url_for('playlist_delete', playlists_id=1), follow_redirects=True)
+        self.assertEqual(response_two.status_code, 200)
+        
+class TestRegistration(TestBase):
+
+    def test_registration(self):
+        """
+        Test that when I register a new user, it redirects me to the login page
+        """
+        with self.client:
+            response = self.client.post(
+                    '/register',
+                    data = dict(
+                        first_name="testfirst",
+                        last_name="testsur",
+                        email="test@test.com",
+                        password="test123",
+                        ),
+                    follow_redirects=True
+                    )
+            self.assertIn(b'testfirst', response.data)
+            self.assertIn(b'testsur', response.data)
+            self.assertIn(b'test@test.com', response.data)
+
+
+class TestLogin(TestBase):
+    def test_Login(self):
+        """
+        test that logging in redirects me to the songs page
+        """
+        with self.client:
+            response = self.client.post(
+                    '/login',
+                    data = dict(
+                        email="admin@admin.com",
+                        password = "admin2016",
+                        ),
+                    follow_redirects=True
+                    )
+            return response
+        self.assertIn(b'admin@admin.com', response.data)
+        self.assertIn(b'admin2016', response.data)
+
+class TestPlaylist(TestBase):
+    def test_playlist(self):
+        """
+        Test that a user can create a playlist
+        """
+        with self.client:
+            logging_in(self)
+            response = self.client.post(
+                    '/playlist',
+                    data = dict(
+                        title="test playlist",
+                        songs_id="test song 1",
+                        songs_id2="test songs 2",
+                        songs_id3="test songs 3",
+                        author="Test author",
+                        ),
+                    follow_redirects=True
+                    )
+            return response
+        self.assertIn(b'test playlist', response.data)
+        self.assertIn(b'test song 1', response.data)
+        self.assertIn(b'test songs 2', response.data)
+        self.assertIn(b'test songs 3', response.data)
+
+
+
+#class TestSong(TestBase):
+ #   def test_song(self):
+  #      """
+   #     test a user can create songs
+    #    """
+     #   with self.client:
+      #      logging_in(self)
+       #     response = self.client.post(
+        #            '/songs',
+         #           data=dict(
+          #              title = "test song title",
+           #             artist = "test artist",
+            #            ),
+             #       follow_redirects=True
+              #      )
+            #return response
+        #self.assertIn(b'test song title', response.data)
+        #self.assertIn(b'test artist', response.data)
+
+"""class TestSongs(TestBase):
+    def test_add_song(self):
+        with self.client:
+            response = self.client.post(
+                    '/login',
+                    data=dict(
+                        email="admin@admin.com",
+                        password="admin2016"
+                        ),
+                    follow_redirects=True
+                    )
+            self.assertEqual(response.status_code, 200)
+
+            responsetwo = self.client.post(url_for('playlist', playlists_id=1), data=dict(title="test song", artist="test artist"))
+            self.assertIn(b'test song', responsetwo.data)
+"""
+class TestUpdate(TestBase):
+    def test_account(self):
+        """
+        test a user can update their account
+        """
+        with self.client:
+            logging_in(self)
+            response = self.client.post(
+                    '/account',
+                    data=dict(
+                        first_name="test",
+                        last_name="test",
+                        email="test@test.com",
+                        ),
+                    follow_redirects=True
+                    )
+            return response
+        self.assertIn(b'test', response.data)
+        self.assertIn(b'test', response.data)
+        self.assertIn(b'test@test.com', response.data)
+
 
